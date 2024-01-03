52a6f7d061777e168bd50e5e25b39dd2d3dc59b0
Akash-Kumar-Sinha <akashkumarsinha304@gmail.com>
2024-01-04 00:45:23 +0530

    first commit

---
+++ src/components/authForm/AuthForm.tsx
@@ -0,0 +1,51 @@
+import { useCallback, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../../@/components/ui/dialog";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+type Variant = "LOGIN" | "REGISTER";
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === "REGISTER") {
+      // Axios Register
+    }
+    if (variant === "LOGIN") {
+      // signin
+    }
+  };
+  return (<div>enjoy2</div>);
+};
+
+export default AuthForm;
