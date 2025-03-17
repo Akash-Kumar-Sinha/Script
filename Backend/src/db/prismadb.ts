9e2b13e41bac0bf4a291d1d48d72c4020b02b9e5
Akash-Kumar-Sinha <akashkumarsinha304@gmail.com>
2025-03-17 19:22:55 +0530

    minor update

--- Backend/src/db/prismadb.ts
+++ Backend/src/db/prismadb.ts
@@ -1,9 +1,21 @@
-import {PrismaClient} from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-declare global{
-    var prisma: PrismaClient | undefined
+declare global {
+  var prisma: PrismaClient | undefined;
 }
 
 const client = globalThis.prisma || new PrismaClient();
-if(process.env.NODE_ENV !== 'production') globalThis.prisma = client;
+
+// Check if the database is connected
+client
+  .$connect()
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database:", error);
+  });
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+
 export default client;
